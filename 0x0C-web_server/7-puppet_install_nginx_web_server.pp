e26102950d5d6b8d5c46c2c47623cf45da1a36de
Kdamzy <kolajidedamilare@gmail.com>
2024-03-26 16:49:52 +0100

    redirection

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,8 +11,7 @@ file {'/var/www/html/index.html':
 }
 
 exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default
-
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.kenny.com/new permanent;' /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
