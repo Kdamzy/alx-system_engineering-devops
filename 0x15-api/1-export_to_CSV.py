25de9806cc2c2783a30efce307cd500342e95fde
Kdamzy <kolajidedamilare@gmail.com>
2024-04-22 12:54:22 +0100

    script that export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,26 +7,31 @@ import sys
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <user_id>")
-        sys.exit(1)
     USER_ID = sys.argv[1]
     url_of_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
-    user_res = requests.get(url_of_user)
-    USERNAME = user_res.json().get('username')
+    #!/usr/bin/python3
+"""Export api to csv"""
+import csv
+import json
+import requests
+import sys
 
-    url_of_task = url_of_user + '/todos'
-    user_res = requests.get(url_of_task)
-    tasks = user_res.json()
 
-    dict_data = {USER_ID: []}
-    for task in tasks:
-        TASK_COMPLETED_STATUS = task.get('completed')
-        TASK_TITLE = task.get('title')
-        dict_data[USER_ID].append({
-                                  "task": TASK_TITLE,
-                                  "completed": TASK_COMPLETED_STATUS,
-                                  "username": USERNAME})
-    """print(dict_data)"""
-    with open('{}.json'.format(USER_ID), 'w') as f:
-        json.dump(dict_data, f)
+if __name__ == '__main__':
+    USER_ID = sys.argv[1]
+    url_of_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
+    res = requests.get(url_of_user)
+
+    user_name = res.json().get('username')
+    task = url_of_user + '/todos'
+    res = requests.get(task)
+    tasks = res.json()
+
+    with open('{}.csv'.format(USER_ID), 'w') as csvfile:
+        for task in tasks:
+            completed = task.get('completed')
+            """Complete"""
+            title_task = task.get('title')
+            """Done"""
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                USER_ID, user_name, completed, title_task))
