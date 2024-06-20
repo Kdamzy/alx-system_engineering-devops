8e480102162da6247262014298b5599d7989065a
Kdamzy <kolajidedamilare@gmail.com>
2024-06-20 21:19:12 +0100

    apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# find out why Apache is returning a 500 error
+# Find out why Apache is returning a 500 error
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
